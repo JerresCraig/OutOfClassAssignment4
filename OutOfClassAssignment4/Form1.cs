6e008447a8f1a7d3ba4958002429b1b0153d08f5
Jerres <Jerres@JERRESLAPTOP>
2019-04-07 19:36:50 -0700

    change 1

--- OutOfClassAssignment4/Form1.cs
+++ OutOfClassAssignment4/Form1.cs
@@ -12,9 +12,114 @@ namespace OutOfClassAssignment4
 {
     public partial class Form1 : Form
     {
+        ttt_Button[,] grid = new ttt_Button[3, 3];
+        bool xTurn = true;
+
         public Form1()
         {
             InitializeComponent();
+            ttt_Button t_button = new ttt_Button();
+            this.Controls.Add(t_button);
+            t_button.Location = new Point(25, 25);
+
+            //initialize the grid with buttons
+            int x = 0, y = 0;
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; r < 3; c++)
+                {
+                    grid[r, c] = new ttt_Button();
+                    this.Controls.Add(grid[r, c]);
+                    grid[r, c].Location = new Point(x, y);
+                    grid[r, c].Click += new EventHandler(button_Click);
+                    y += 100;
+                }
+                x += 100;
+                y = 0;
+            }
+        }
+        //win conditions
+        private bool checkForWin()
+        {
+            if (grid[0, 0].Text == grid[0, 1].Text &&
+                grid[0, 1].Text == grid[0, 2].Text &&
+                grid[0, 0].Text != "*")
+                return true;
+            else if
+                (grid[1, 0].Text == grid[1, 1].Text &&
+                 grid[1, 1].Text == grid[1, 2].Text &&
+                 grid[1, 0].Text != "*")
+                return true;
+            else if
+                (grid[2, 0].Text == grid[2, 1].Text &&
+                 grid[2, 1].Text == grid[2, 2].Text &&
+                 grid[2, 0].Text != "*")
+                return true;
+            else if
+                (grid[0, 0].Text == grid[1, 0].Text &&
+                 grid[1, 0].Text == grid[2, 0].Text &&
+                 grid[0, 0].Text != "*")
+                return true;
+            else if
+                (grid[0, 1].Text == grid[1, 1].Text &&
+                 grid[1, 1].Text == grid[2, 1].Text &&
+                 grid[0, 1].Text != "*")
+                return true;
+            else if
+                (grid[0, 2].Text == grid[1, 2].Text &&
+                 grid[1, 2].Text == grid[2, 2].Text &&
+                 grid[0, 2].Text != "*")
+                return true;
+            else if
+                (grid[0, 0].Text == grid[1, 1].Text &&
+                 grid[1, 1].Text == grid[2, 2].Text &&
+                 grid[0, 0].Text != "*")
+                return true;
+            else if
+                (grid[2, 0].Text == grid[1, 1].Text &&
+                 grid[1, 1].Text == grid[0, 2].Text &&
+                 grid[2, 0].Text != "*")
+                return true;
+            else
+                return false;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            //capture the sender
+            ttt_Button t = (ttt_Button)sender;
+            if (xTurn)
+            {
+                t.BackColor = Color.RoyalBlue;
+                t.Text = "X";
+            }
+            else
+            {
+                t.BackColor = Color.Gold;
+                t.Text = "O";
+            }
+            xTurn = !xTurn;
+
+            if (checkForWin())
+            {
+                MessageBox.Show("You are a Winner!");
+                //reset board
+                int x = 0, y = 0;
+                for (int r = 0; r < 3; r++)
+                {
+                    for (int c = 0; r < 3; c++)
+                    {
+                        grid[r, c] = new ttt_Button();
+                        this.Controls.Add(grid[r, c]);
+                        grid[r, c].Location = new Point(x, y);
+                        grid[r, c].Click += new EventHandler(button_Click);
+                        y += 100;
+                    }
+                    x += 100;
+                    y = 0;
+
+                }
+            }
         }
     }
 }
---
+++ OutOfClassAssignment4/ttt_Button.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OutOfClassAssignment4
+{
+    class ttt_Button : Button
+    {
+        public static int Btn_Size = 100;
+
+        public ttt_Button()
+        {
+            this.Width = this.Height = Btn_Size;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.Text = "*";
+            this.FlatStyle = FlatStyle.Flat;
+            this.Font = new System.Drawing.Font("Consolas", 25);
+        }
+    }
+}
